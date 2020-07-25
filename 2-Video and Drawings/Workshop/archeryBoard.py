13d5c45eae9ae034671a3e339de1daa2a28d275a
Gokul Manohar <gokulmanohar7@gmail.com>
2020-07-25 23:07:17 +0530

    Update archeryBoard.py

--- 2-Video and Drawings/Workshop/archeryBoard.py
+++ 2-Video and Drawings/Workshop/archeryBoard.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 
 img=np.full((500, 500, 3), 255, np.uint8) #creates a white background image
-cv2.circle(img, (250,250), 250, (245, 135, 66), -1)
-cv2.circle(img, (250,250), 200, (111, 219, 29), -1)
-cv2.circle(img, (250,250), 150, (0, 219, 255), -1)
+cv2.circle(img, (250,250), 250, (245, 135, 66), -1) #blue circle
+cv2.circle(img, (250,250), 200, (111, 219, 29), -1) #green circle
+cv2.circle(img, (250,250), 150, (0, 219, 255), -1) #yellow circle
 cv2.imshow("Archery Board", img)
 cv2.waitKey(0)
 cv2.destroyWindow(("Archery Board"))
